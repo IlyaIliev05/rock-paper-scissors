b27cc05ee05666de85813d7ae4f6c36fe7407cdf
IlyaIliev05 <ilievilia15@gmail.com>
2022-07-05 23:36:26 +0000

    Add playGame, checkWinner, displayResult functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,28 @@
-/** 
- * Declaring constants for DOM elements
- * and choices (rock, paper, scissors)
- */
-
-const buttons = document.getElementsByClassName('set-game');
-const playerResult = document.getElementById('player-score');
-const computerResult = document.getElementById('computer-score');
-const playerImg = document.getElementById('player-image');
-const computerImg = document.getElementById('computer-image');
-const messages = document.getElementById('messages');
-const choices = ["rock", "paper", "scissors"];
+const fullGame = [
+    ["draw", "player wins", "computer wins"],
+    ["player wins", "draw", "computer wins"],
+    ["computer wins", "player wins", "draw"]
+]
+let playerResult = document.getElementById('player-score');
+let computerResult = document.getElementById('computer-score');
+let messages = document.getElementById('messages');
+let choices = ["rock", "paper", "scissors"];
 
 /**
- * Adding Event listeners to all the controls
+ * Adding Event listeners to all of the buttons
  */
 
-for (let button of buttons) {
-    button.addEventListener("click", function(){
-        let playerChoice = this.getAttribute('data-choice');
-        playGame(playerChoice);
-    });
-}
+document.addEventListener("DOMContentLoaded", function () {
+
+    let buttons = document.getElementsByClassName('set-game');
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            let playerChoice = this.getAttribute('data-choice');
+            playGame(playerChoice);
+        });
+    }
+});
 
 /**
  * Main game function. Agrees to receive one parameter, which 
@@ -28,19 +30,26 @@ for (let button of buttons) {
  */
 
 function playGame(playerChoice) {
+
+    let playerImg = document.getElementById('player-image');
     playerImg.src = `assets/images/${choices[playerChoice]}.png`;
     playerImg.alt = choices[playerChoice];
 
-    let computerChoice = Math.floor(Math.random() * 3 );
+    let computerChoice = Math.floor(Math.random() * 3);
 
+    let computerImg = document.getElementById('computer-image');
     computerImg.src = `assets/images/${choices[computerChoice]}.png`;
     computerImg.alt = choices[computerChoice];
 
-    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
-
-    updateScores(result);
+    let result = checkWinner(playerChoice, computerChoice);
+    displayResult(result, messages);
+    console.log("result: ", result);
 }
 
 function checkWinner(playerChoice, computerChoice) {
-    
+    return fullGame[playerChoice][computerChoice];
+}
+
+function displayResult(result, elem) {
+    elem.innerText = result;
 }
